64bb26dfcb6c0287852140953b63d0f61c166a92
azeighami <64235411+azeighami@users.noreply.github.com>
2020-07-07 18:19:05 -0400

    Add files via upload

--- corn_stover_ethanol.py
+++ corn_stover_ethanol.py
@@ -10,12 +10,17 @@ import pandas as pd
 import Land_Dedication_Seeding
 import Irrigation
 import Fertilization
+import Available_Corn_Stover
+import Truck
+import Comminution
 
 ###################################
 # CONVERSIONS
 ha_to_acre = 2.47105 # Hectares to Acres
 gal_to_acre_feet = 3.07e-6 # Gallons to Acre-feet
 lb_to_kg= 0.453515 # pounds to kilograms
+Bushels_corn_to_kg = 25.4
+Miles_to_km = 1.60934
 
 
 ###################################
@@ -30,7 +35,14 @@ Lime_tretment = 183.04 # kg of lime per acre
 
 Herbicide_Atrazibe = 1.082 #lb of Atrezine per acre 
 
+hours_per_ha_laber = 2.965
+L_diesel_per_ha = 56.124
 
+field_to_biorefinary = 10 # miles
+Truck_Capasity = 20 #tonnes
+
+Comminution_energy = 0.1404 # MJ per kg stover
+Percent_to_hammer_mill = 97 # percent of corn stover goes into hammer mill is comminuted 
 ###################################
 # AGRICULTURE 100s
 
@@ -53,16 +65,29 @@ Lime_per_ha = Lime_tretment * ha_to_acre
 #160 - Herbicide 
 Herbicide_per_ha = Herbicide_Atrazibe * ha_to_acre * lb_to_kg
 
-#170 _ Crop Operations
+#170 - Crop Operations
+Laber_h_per_ha = hours_per_ha_laber
+L_deisel= L_diesel_per_ha
+
+#180 - Harvest Yeild 
+kg_corn_grain_per_ha = bu_per_acre * Bushels_corn_to_kg * ha_to_acre
+corn_stover_per_ha = kg_corn_grain_per_ha * 0.8867
+kg_stover_per_ha = Available_Corn_Stover.Stover(corn_stover_per_ha, arable_land)
+
+#190 - Storage and Transportation
+transportation = field_to_biorefinary * Miles_to_km #km
+num_of_trucks = Truck.TRK (kg_stover_per_ha, Truck_Capasity)
 
 ###################################
-#  
 
+# BIOPROCESS NAD CONVERSION 200 - 600
+ 
+# 200 - Size Reduction Process - Comminution 
+comm = Comminution.COMM (kg_stover_per_ha , Comminution_energy , Percent_to_hammer_mill)
+Comminution_Electricity = comm [1] #MJ/ha
+kg_comm_stover_ha = comm [0] # kg comminuted stover per ha
 
-
-
-
-
+# 300 - Dilute acid hydrolysis 
 
 
 ###################################
