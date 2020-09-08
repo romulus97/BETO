03751a88e589b3072d487ee64a285ea68872a0d2
Jordan Kern <jkern@ncsu.edu>
2020-09-08 00:13:18 -0400

    update

--- platypus_BETO.py
+++ platypus_BETO.py
@@ -31,6 +31,10 @@ bu_per_acre_C_yield = df_geo['C_yield'].values
 df_dist = pd.read_excel('geo.xlsx',sheet_name='distance_lookup',header=0)
 dist_map = np.zeros((len(counties),len(counties)))
 
+# Simple refinery case (remove later)
+refinery_idx = int(np.round(np.float(np.random.rand(1)*98),decimals=0))
+ 
+
 # convert look-up table to matrix
 for i in range(0,len(counties)):
     c1 = counties[i]
@@ -54,14 +58,15 @@ def simulate(
         LC = land_costs, # land costs per county
         C_Y = bu_per_acre_C_yield, # corn yield per ha
         LL = land_limits, # land limits
-        DM = dist_map # distance mapping
+        DM = dist_map, # distance mapping
+        R_idx = refinery_idx # location of refinery in simple case
         ):
     
     CS_kg = 0
     CS_costs = 0
     CS_travel = 0
     Constraints = []
-    
+       
     #Cultivation   
     for i in range(0,len(counties)):
         
@@ -71,9 +76,8 @@ def simulate(
         
     # Flow to refinery
     
-    # Simple case - specify location of single refinery of infinite size
-        refinery_idx = 25
-        CS_travel += DM[i,refinery_idx]*(20000/CS)
+    # Simple case - random location of single refinery of infinite size
+        CS_travel += DM[i,refinery_idx]*(CS/20000)
     
     # Processing        
     
@@ -81,7 +85,8 @@ def simulate(
         Constraints.append(vars[i] - LL[i])
         Constraints.append(-vars[i])        
     
-    Constraints.append(150-CS_kg)
+    Constraints.append(4490000-CS_kg)
+    Constraints.append(CS_kg - 4510000)
     Constraints = list(Constraints)
     
     return [CS_costs, CS_travel], Constraints
@@ -92,13 +97,14 @@ def simulate(
 #####################################################################
 
 # Define Platypus problem
-problem = Problem(len(counties),2,199)
+problem = Problem(len(counties),2,200)
 problem.types[:] = Real(0,100)
 problem.constraints[:] = "<=0"
 problem.function = simulate
 algorithm = NSGAII(problem)
+
 # Evaluate function # of times
-algorithm.run(100000)
+algorithm.run(500000)
 
 stop = time.time()
 elapsed = (stop - start)/60
@@ -110,58 +116,79 @@ print(mins)
 ##########           OUTPUTS                 ########################
 #####################################################################
 
-# display the tradeoff frontier
+# limit evalutaion to 'feasible' solutions
 feasible_solutions = [s for s in algorithm.result if s.feasible]
+
+obj1 = []
+obj2 = []
+
+for s in feasible_solutions:
+    obj1.append(s.objectives[0])
+    obj2.append(s.objectives[1])
     
+
+# find solutions' standardized distance to ideal (origin)
+distance_to_origin_pct = []
+
+for s in feasible_solutions:
+    d = ((s.objectives[0]/max(obj1))**2 + (s.objectives[1]/max(obj2))**2)**0.5
+    distance_to_origin_pct.append(d)
+
+    
+# select range of standardized solutions to map
+sorted_distance = np.sort(distance_to_origin_pct)
+idx = []
+idx_obj1 = []
+for i in range(0,99,9):
+    idx.append(distance_to_origin_pct.index(sorted_distance[i]))
+    idx_obj1.append(obj1[i])
+    
+# display the tradeoff frontier    
 import matplotlib.pyplot as plt
 
 plt.scatter([s.objectives[0] for s in feasible_solutions],
             [s.objectives[1] for s in feasible_solutions])
 
+for i in idx:
+    
+    plt.scatter(obj1[i],obj2[i],s=36,c='red')
+    
 plt.xlabel("Costs ($)")
 plt.ylabel("Distance (km)")
 plt.show()
 
+
 # visualize (map) solutions
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
 import plotly.express as px
+import plotly.io as pio
+pio.renderers.default='jpg'
 
-# Min Cost
-s1 = np.array(feasible_solutions[0].variables)
-df_results = pd.DataFrame()
-df_results['fips'] = fips
-df_results['CS_acres'] = s1
-fig1 = px.choropleth(df_results, geojson=counties, locations='fips', color='CS_acres',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 5),
-                           scope="usa",
-                           labels={'CS_acres':'Corn Stover Acres'}
-                          )
-fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig1.show()
+# plot in descending order from Obj1
+df_combined = pd.DataFrame()
+df_combined['idx'] = idx
+df_combined['obj1'] = idx_obj1
+df_sorted = df_combined.sort_values(by='obj1',ascending=False).reset_index(drop=True)
 
-# Min distance
-s2 = np.array(feasible_solutions[100].variables)
-df_results = pd.DataFrame()
-df_results['fips'] = fips
-df_results['CS_acres'] = s2
-fig2 = px.choropleth(df_results, geojson=counties, locations='fips', color='CS_acres',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 5),
-                           scope="iowa",
-                           labels={'CS_acres':'Corn Stover Acres'}
-                          )
-fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig2.show()
-
-# # Compromise
-# fig3 = px.choropleth(df_geo, geojson=counties, locations='fips', color='CS_acres',
-#                            color_continuous_scale="Viridis",
-#                            range_color=(0, 12),
-#                            scope="iowa",
-#                            labels={'CS_acres':'Corn Stover Acres'}
-#                           )
-# fig3.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-# fig3.show()
-
+for i in range(0,len(df_sorted)):
+    
+    j = df_sorted.loc[i,'idx']
+    s = np.array(feasible_solutions[j].variables)
+    df_results = pd.DataFrame()
+    df_results['fips'] = fips
+    df_results['CS_acres'] = s
+    df_results['fips'] = df_results['fips'].apply(str)
+    fig = px.choropleth(df_results, geojson=counties, locations='fips', color='CS_acres',
+                               color_continuous_scale="Viridis",
+                               range_color=(0, 50),
+                               scope="usa",
+                               labels={'CS_acres':'Corn Stover Acres'}
+                              )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.show()
 
 
