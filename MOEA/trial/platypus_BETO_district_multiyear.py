bc867e7472e52ff4bcc00110e7cca8ecc33b54b4
Ece Ari Akdemir <84621380+eceariakdemir@users.noreply.github.com>
2021-10-05 18:16:40 -0400

    update

--- MOEA/trial/platypus_BETO_district_multiyear.py
+++ MOEA/trial/platypus_BETO_district_multiyear.py
@@ -5,13 +5,13 @@ Created on Fri Jul  3 16:20:20 2020
 """
 
 from platypus import GDE3, Problem, Real
-from pyborg import BorgMOEA
 import random
 from random import randint
 import pandas as pd
 import numpy as np
 import time
-import corn_stover_processing_V as CS_processing
+import corn_grain_processing as CG_processing
+
 
 start = time.time()
 version = 'district'
@@ -21,25 +21,55 @@ version = 'district'
 #####################################################################
 
 # import district level data
+# df_yield = pd.read_excel('normalized_63_year_corn_yield.xlsx',header=0,engine='openpyxl') #contains yearly yield data for corn in each ag_district
 df_geo = pd.read_excel('combined_pivot_excel_electricity.xlsx',header=0, engine='openpyxl') #contains every eg_district code 
-
-districts = list(df_geo['STASD_N']) # list of ag_district code
-land_costs = df_geo.loc[:,'land_costs-$/ha'].values # $ per ha
-land_limits = df_geo['land_limits_ha'].values # county ag production area in acres
 cost = list(df_geo['CG_cost_per_ha'])
-C_yield = df_geo.iloc[:,8:].values  # Corn Grain yield as kg/ha
+districts = list(df_geo['STASD_N']) # list of ag_district code
 
 #specify grouping
 groups = 20
 
 #district-to-hub data
-df_D2H = pd.read_excel('AgD2H_48_cb.xlsx',header=0, engine='openpyxl') # contains travel time and travel distance
+filename = 'AgD2H_48_cb.xlsx' # contains travel time and travel distance - these are not changed with year
+df_D2H = pd.read_excel(filename,header=0, engine='openpyxl')
 c = list(df_D2H['STASD_N']) # list of ag_district code in filename excel sheet 
+
+#eliminate districts that don't appear in both lists - (US_ag_district_geodata contains each ag_district code in the USA but we want to use only corn belt)
+for i in districts: # districts = whole list of ag_district 
+    idx = districts.index(i)
+    if i in c: # c = ag_dist code only located in corn belt
+        pass
+    else:
+        df_geo = df_geo.drop(index=idx) #deleting the rest of the ag_district code where is located out of corn belt
+
+df_geo = df_geo.reset_index(drop=True) # fixing the index number because we drop some of them and the numbers are not going by orderly. 
+# fips = df_geo['fips'].values
+districts = list(df_geo['STASD_N'])# new district list which is only contains corn belt ag_districts
+land_costs = df_geo.loc[:,'land_costs-$/ha'].values # $ per ha
+land_limits = df_geo['land_limits_ha'].values # county ag production area in acres
+years = range(1958,2021)
+listyears =[]
+
+for year in years :
+    listyears.append(str(year))
+
+# Corn Grain yield
+C_yield = df_geo.iloc[:,8:].values  #yield in bushels per acre
+
+
+########################################################################
+#########        PRE-PROCESSING       ##################################
+########################################################################
+
+################################
+# Convert to function inputs
 
 # Hub grouping information, distance look-up tables
 dist_D2H = []
 district_hubs = []
 for d in districts:
+    
+    # idx = districts.index(d)
     
     # distance from each district to pre-defined hub
     dist_D2H.append(df_D2H.loc[df_D2H['STASD_N']==d,'travel_dist_km'].values[0])
@@ -47,22 +77,16 @@ for d in districts:
     # list of hubs assigned to each district
     district_hubs.append(df_D2H.loc[df_D2H['STASD_N']==d,'destinationID'].values[0]) # the whole corn belt divided 18 hubs abd this defines which ag_dist found under which hub
 
-#hub-to-hub data
-df_H2H =pd.read_excel('AgD_48g_H2H_cb.xlsx', header=0, engine='openpyxl')
-hubs = list(df_H2H['OriginID'].unique()) # there is 18 hub 
 
-years = range(1958,2021)
-listyears =[]
-
-for year in years :
-    listyears.append(str(year))
-
-########################################################################
-#########        PRE-PROCESSING       ##################################
-########################################################################
-
+# Pre-define location of refineries
 # put a number > 0 and < number of hubs if desired; if not, problem defaults to full list of hubs
 num_refineries = 0
+
+#hub-to-hub data
+filename = 'AgD_48g_H2H_cb.xlsx'
+df_H2H = pd.read_excel(filename,header=0, engine='openpyxl')
+hubs = list(df_H2H['OriginID'].unique()) # there is 18 hub 
+
 locations = hubs
     
 ################################
@@ -77,38 +101,45 @@ for i in range(0,len(hubs)):
         c2 = locations[j]
         dist_map[i,j] = df_H2H.loc[(df_H2H['OriginID']==c1) & (df_H2H['DestinationID']==c2),'Total_Kilometers'] 
 
-map_D2H = np.zeros((len(districts), len(hubs))) # 107 district and 18 hubs 
+map_D2H = np.zeros((len(districts), len(hubs))) # I don't understand what we are trying to define 
 
 # convert look-up table to distance matrix #when 50 is indexed 49 is not valid because of missing hubs
 for i in range(0,len(districts)):
     h = hubs.index(int(district_hubs[i])) # int(county_hubs[i]) - 1
-    map_D2H[i,h] = 1    # represnet which district located which hub 
+    map_D2H[i,h] = 1    
 
-
-# #########################################################
-# # Identify quota as % of maximum theoretical production
-# import multiyear_quota
-# p=.05
-# Q_min, Q_series = multiyear_quota.QD(districts,locations)
-# quota = Q_min*p #quota in biomass in kilograms as opposed to ethanol
 
 #########################################################
 # Identify quota as % of maximum theoretical production
-import deneme_quota
+import multiyear_quota
 p=.05
-quota, UB , v_test = deneme_quota.QD(districts,locations,p)
-quota = quota[0]
+Q_min, Q_series = multiyear_quota.QD(districts,locations)
+quota = Q_min*p #quota in biomass in kilograms as opposed to ethanol
 
+# FIX QUOTA TO BE MULTIYEAR
+ 
 #####################################################################
 ##########           FUNCTION DEFINITION     ########################
 #####################################################################
+   
+###################################
+# CONVERSIONS
+ha_to_acre = 2.47105 # Hectares to Acres
+lb_to_kg= 0.453515 # pounds to kilograms
+kg_to_L_Ethanol = 1.273723
 
+# NOTE: MAKE SURE OF CONVERSIONS BELOW -- VALUES ABOVE NOW APPEAR ALL IN ACRES
+    
+# # test function
+# import f_test
+# obj1,obj2,product = f_test.test(v,reduced_land_costs,reduced_C_yield,reduced_land_limits, dist_map,map_C2H,dist_C2H,locations,hubs,quota)
+# simulation model (function to be evaluated by MOEA)
 def simulate(
     
     
         vars, # cultivation hectares per county, hub-to-hub biomass flows
         LC = land_costs, # land costs per county
-        C_Y = C_yield, # corn yield kg/ha
+        C_Y = C_yield, # corn yield per acre
         LL = land_limits, # land limits
         CG_cost_per_ha = cost,  #corn grain cost per hectare 
         DM = dist_map, # hub to hub distances
@@ -130,6 +161,13 @@ def simulate(
     CG_prod_total = np.zeros((len(years), 1))
     CG_ethanol_total = np.zeros((len(years),1))
     # CS_travel_opex = np.zeros((len(years, 1)))
+    
+    # CS_flow_matrix = np.zeros((len(hubs),len(locations)))
+    # CS_D2H_prod = np.zeros((num_c,num_h))
+    # CS_flow = 0
+    # CS_refinery_kg = 0
+    # CS_ethanol = np.zeros((num_l,1))
+    # CS_refinery_capex = 0
 
     # Capital costs (need to expand)
     L = np.array(LC) #$/acre
@@ -147,17 +185,103 @@ def simulate(
     for year in years:
         i = years.index(year)
         Y = C_Y[:,i]
-
+        
+        # Constraints = [] # constraints
+        
+        # Per ha values (need to expand) # this is where we would put in code from Jack 
+        
+        ##############################
+        # Cultivation and Harvesting
+    
+        # Operating costs (need to expand)
+        #harvesting = 38.31*ha_to_acre # $ per ha
+        
+        # d2h_map = np.array(D2H_map)
+        # Automatic flow to pre-processing hub
+        # CS_D2H_prod = d2h_map * np.transpose(np.array([(CS_per_ha * v[0:num_c])]*num_h)) #kg = kg*ha/ha
         CG_prod = np.sum(v[0:num_c]*Y)
         CG_prod_total[i] = CG_prod
         
+        # CS_cultivation_opex += np.sum(CS_per_ha*v[0:num_c]*(lb_to_kg)*(1/1500)*0.50*D2H)
+       
+        # Cultivation constraints (land limits) #PUT OUTSIDE OF LOOP 
+        # for i in range(0,len(LC)):
+        #     Constraints.append(vars[i] - LL[i] - 5) #allow some slack in DVs
+            
+        # LL_cons = np.subtract(np.subtract(v[0:len(LC)], LL), 5)
+        # Constraints.extend(LL_cons.tolist())
+       
+        ################################
+            
+        # ref = (len(LC) + (len(hubs) - 1)*len(locations) + (len(locations) - 1)) + 1
+       
+        # # Mass transfer (1Ms kg of CS) from hub 'j' to hub 'k'
+        # CS_flow_matrix = v[num_c:].reshape((num_h,num_l)) #kg
+        
+        # Travel costs in bale-miles
+        # CS_travel_opex = np.sum(DM*CS_flow_matrix*((lb_to_kg)*(1/1500)*0.50)) # km*kg*
+        
+        # #Flow to refinery
+        # for j in range(0,len(hubs)):
+            
+        #     for k in range(0,len(locations)):
+            
+        #         # Mass transfer (1Ms kg of CS) from hub 'j' to hub 'k'
+        #         CS_flow_matrix[j,k] = CS_flow_matrix[j,k] + vars[len(LC) + j*len(locations) + k] 
+                
+        #         # Travel costs in bale-miles
+        #         CS_travel_opex += DM[j,k]*CS_flow_matrix[j,k]*(lb_to_kg)*(1/1500)*0.50 
+    
+        #for j in range(0,len(hubs)):
+        # P = np.array(CS_C2H_prod)
+        # F = np.array(CS_flow_matrix)
+       
+        # Hubs collect biomass from counties
+        # CS_hub_kg = np.sum(CS_D2H_prod, axis=0) #kg
+        
+        # Transportation constraints (all delivery from hub 'j' must be <= mass produced)
+        # CS_flow = np.sum(CS_flow_matrix, axis=1) #kg
+        # flow_constraints = CS_flow - CS_hub_kg - 5 #allow some slack in DVs #kg
+        
+        # for j in range(0,len(hubs)):
+        #     Constraints.append(flow_constraints[j]) 
+        
+        # Constraints.extend(flow_constraints.tolist())
         
         ###############################
         # Refinery
+       
+        #for k in range(0,len(locations)):
+              
+        # Find total mass received at hub 'l'
+        # CS_refinery_kg = np.sum(CS_flow_matrix, axis=0) #kg
         
+        # # Only allow plants above certain scale
+        # filter_flows = np.array(list(map(int,(CS_refinery_kg/(5563*142))>1000)))
+        # new_kg = filter_flows*CS_refinery_kg
+        # scale = filter_flows*(CS_refinery_kg/(5563*142))
+                
         # Ethanol produced at refinery at hub 'l'
-        CG_ethanol = CS_processing.sim(CG_prod) #L
+        CG_ethanol = CG_processing.sim(CG_prod) #L
         CG_ethanol_total[i] = CG_ethanol
+        # scale = CS_refinery_kg/(5563*142)
+        
+        # for k in range(0,len(locations)):
+            # if scale[k] < 1000:
+            #     CS_refinery_capex += 25000000*scale[k]
+            # elif scale[k] >= 1000 and scale[k] < 5000:
+            #     CS_refinery_capex += 12500000*scale[k]
+            # elif scale[k] >= 5000 and scale[k] < 10000:
+            #     CS_refinery_capex += 7500000*scale[k]    
+            # else:
+            #     CS_refinery_capex += 500000*scale[k] 
+            
+    
+        # CS_refinery_capex = 400000000*(scale)**.6
+        
+        # Sets ethanol production quota (L)
+        # Constraints.append(Q - np.sum(CS_ethanol)-5) #L
+        # Constraints.append(sum(CS_ethanol)[0] - Q*1.05) #remove upper contraint 
         
         if (Q - CG_ethanol) < 0:
        
@@ -176,6 +300,10 @@ def simulate(
     biomass_cost = CG_cultivation_capex + CG_cultivation_opex
     min_shortfall = max(Z)
     return [biomass_cost, min_shortfall], Constraints ##
+    
+#return [biomass_cost,   np.sum(v[0:num_c]), np.sum(CS_refinery_capex), CS_travel_opex], Constraints
+
+
 
 ####################################################################
 #########           MOEA EXECUTION          ########################
@@ -203,7 +331,10 @@ problem.constraints[:] = "<=0"
 problem.function = simulate
 
 # What algorithm?
-algorithm = BorgMOEA(problem, epsilons=10)
+algorithm = GDE3(
+    problem=problem,
+    population_size=250,
+    )
 
 # Evaluate function # of times
 algorithm.run(500000)
